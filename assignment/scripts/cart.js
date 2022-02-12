f6a0710e52a20b74bf089fe0ce6e71f94a19842a
Jonathan Palalay <jonopalalay@gmail.com>
2022-02-11 23:58:11 -0600

    first savepoint

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,19 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = []
+
+function addItem(item){
+  basket.push(item);
+  return true;
+}
+
+console.log(`Basket is ${basket}`);
+console.log('Adding apples (expect true)', addItem('apples'));
+console.log(`Basket is now ${basket}`);
+
+
+function listItems(basket)
+  for (let i=0; i<basket.length; i++){
+      console.log ('Items in basket:', basket[i]);
+}
