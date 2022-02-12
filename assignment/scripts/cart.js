238ff47604ed325d66c352462ae2d6dd4a32915f
Jonathan Palalay <jonopalalay@gmail.com>
2022-02-12 13:15:41 -0600

    savepoint 2

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -13,7 +13,20 @@ console.log('Adding apples (expect true)', addItem('apples'));
 console.log(`Basket is now ${basket}`);
 
 
-function listItems(basket)
+function listItems(){
   for (let i=0; i<basket.length; i++){
-      console.log ('Items in basket:', basket[i]);
+      console.log (`Items in basket:', ${basket[i]} `);
+
+    }
 }
+
+console.log('Adding orange (expect true)', addItem('orange'));
+console.log(basket.length)
+console.log(listItems() )
+
+
+function empty(){
+  for (let j=0; j<basket.length; i--){
+    
+}
+console.log(basket.length)
